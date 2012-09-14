bec310c89b0236436c4ce67b4e7ac648d00a55b2
saravanan-va <saravanan.varadarajan@photoninfotech.net>
2012-09-14 19:58:50 +0530

    [artf533065]removed unused imports

--- dotnet-maven-plugin/src/main/java/com/photon/phresco/plugins/DotNetDeploy.java
+++ dotnet-maven-plugin/src/main/java/com/photon/phresco/plugins/DotNetDeploy.java
@@ -29,22 +29,18 @@ import java.util.List;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.StringUtils;
 import org.codehaus.plexus.util.cli.CommandLineException;
 import org.codehaus.plexus.util.cli.Commandline;
 
-import com.photon.phresco.commons.BuildInfo;
 import com.photon.phresco.exception.PhrescoException;
 import com.photon.phresco.framework.PhrescoFrameworkFactory;
 import com.photon.phresco.framework.api.ProjectAdministrator;
 import com.photon.phresco.model.SettingsInfo;
 import com.photon.phresco.plugin.commons.PluginConstants;
-import com.photon.phresco.plugin.commons.PluginUtils;
 import com.photon.phresco.util.ArchiveUtil;
 import com.photon.phresco.util.ArchiveUtil.ArchiveType;
 import com.photon.phresco.util.Constants;
-import com.photon.phresco.util.Utility;
 
 /**
  * Goal which deploys the DotNet webapp project
