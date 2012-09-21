afa965de5dd2ab6ffabdcdf1037d9ba07d83ef18
saravanan-va <saravanan.varadarajan@photoninfotech.net>
2012-09-21 13:05:53 +0530

    [ artf536738] added utility method for packaging java releated projects

--- java-maven-plugin/src/main/java/com/photon/phresco/plugins/JavaPackage.java
+++ java-maven-plugin/src/main/java/com/photon/phresco/plugins/JavaPackage.java
@@ -251,9 +251,6 @@ public class JavaPackage extends AbstractMojo implements PluginConstants {
 	}
 	
 	private void executeMvnPackage() throws MojoExecutionException {
-		BufferedReader bufferedReader = null;
-		boolean errorParam = false;
-		try {
 			getLog().info("Packaging the project...");
 			StringBuilder sb = new StringBuilder();
 			sb.append(MVN_CMD);
@@ -263,23 +260,7 @@ public class JavaPackage extends AbstractMojo implements PluginConstants {
 			sb.append(MVN_PHASE_PACKAGE);
 			sb.append(STR_SPACE);
 			sb.append(SKIP_TESTS);
-
-			bufferedReader = Utility.executeCommand(sb.toString(), baseDir.getPath());
-			String line = null;
-			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println(line); // do not use getLog() here as this line already contains the log type.
-				if (line.startsWith("[ERROR]")) {
-					errorParam = true;
-				}  
-			}
-			if (errorParam) {
-				throw new MojoExecutionException("Compilation Failure ... ");
-			}
-		} catch (IOException e) {
-			throw new MojoExecutionException(e.getMessage(), e);
-		} finally {
-			Utility.closeStream(bufferedReader);
-		}
+			Utility.executeStreamconsumer(sb.toString());
 	}
 
 	private boolean build() throws MojoExecutionException {
