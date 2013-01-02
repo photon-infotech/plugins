39a0e1b1e145213f4eca277ca3a0516edf7fc526
mahesh-p <mahesh.prathapaneni@photoninfotech.net>
2013-01-02 10:45:42 +0530

    Consumer site Plugin changes

---
+++ consumer-site-plugin/src/main/java/com/photon/phresco/util/UtilConstant.java
@@ -0,0 +1,12 @@
+package com.photon.phresco.util;
+
+import java.io.File;
+
+public class UtilConstant {
+
+	public static final String zipFileLocation = ".\\package_holder";
+	public static final String zipFileName = "\\Mobility_Phase2_Enhancement_ThemePackage_CNC.zip";
+	public static final String unzippedFolderLocation = "./package_holder";
+	public static final String unzippedFolderName = "/Mobility_Phase2_Enhancement_ThemePackage_CNC";
+	public static final String manifestFileName = "/theme-manifest.xml";
+}
