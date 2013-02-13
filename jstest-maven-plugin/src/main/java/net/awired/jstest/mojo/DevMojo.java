31b33a91cc5fc42db6fa5f56b7f302fa665e4ea0
Bharat Kumar Radha <bharatkumar.radha@photoninfotech.net>
2013-02-13 21:40:48 +0530

    spring cross domain issue fixed

--- jstest-maven-plugin/src/main/java/net/awired/jstest/mojo/DevMojo.java
+++ jstest-maven-plugin/src/main/java/net/awired/jstest/mojo/DevMojo.java
@@ -1,5 +1,8 @@
 package net.awired.jstest.mojo;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import net.awired.jstest.mojo.inherite.AbstractJsTestMojo;
 import net.awired.jstest.resource.ResourceDirectory;
@@ -7,8 +10,15 @@ import net.awired.jstest.resource.ResourceResolver;
 import net.awired.jstest.server.JsTestServer;
 import net.awired.jstest.server.handler.JsTestHandler;
 import net.awired.jstest.server.handler.ResultHandler;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.cli.CommandLineException;
+import org.codehaus.plexus.util.cli.Commandline;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * @goal dev
@@ -37,9 +47,48 @@ public class DevMojo extends AbstractJsTestMojo {
                     new ArrayList<ResourceDirectory>());
             //TODO remove resultHandler creation we dont need it here
             ResultHandler resultHandler = new ResultHandler(getLog(), null, buildTestType(resourceResolver));
-            jsTestServer.startServer(new JsTestHandler(resultHandler, getLog(), resourceResolver,
+            
+            JsTestHandler jsTestHandler = new JsTestHandler(resultHandler, getLog(), resourceResolver,
                     buildAmdRunnerType(), buildTestType(resourceResolver), true, getLog().isDebugEnabled(),
-                    getAmdPreloads(), getTargetSourceDirectory()));
+                    getAmdPreloads(), getTargetSourceDirectory());
+            
+            Handler[] handlers = new Handler[2];
+            handlers[0] = jsTestHandler;
+
+            if (isPackBeforeTest()) {
+                getLog().info("Package Started");
+                Commandline cmdLine = new Commandline("mvn package -Pjava -DskipTests");
+                cmdLine.setWorkingDirectory(".");
+                try {
+                    Process process = cmdLine.execute();
+                    InputStream inputStream = process.getInputStream();
+                    StringWriter writer = new StringWriter();
+                    IOUtils.copy(inputStream, writer);
+                    String output = writer.toString();
+                    if (output.contains("[INFO] BUILD SUCCESS")) {
+                        getLog().info("Packaged successfully");
+                        WebAppContext webAppContext = new WebAppContext();
+                        webAppContext.setWar(getWarTargetDir());
+
+                        //webAppContext.setDescriptor("WEB-INF/web.xml");
+                        //webAppContext.setResourceBase("/Users/bharatkumarradha/Downloads/BT/bt/src/main/webapp");
+
+                        webAppContext.setContextPath("/" + context);
+                        webAppContext.setParentLoaderPriority(true);
+                        handlers[1] = webAppContext;
+                    } else {
+                        getLog().info("Package Failed");
+                    }
+                } catch (CommandLineException e1) {
+                    throw new MojoFailureException("Do not package the application to test");
+                }
+               
+            }
+
+            HandlerCollection handlerCollect = new HandlerCollection();
+            handlerCollect.setHandlers(handlers);
+            jsTestServer.startServer(handlerCollect);
+
             getLog().info(String.format(INSTRUCTION_FORMAT, getDevPort(), getSourceDir(), getTestDir()));
             jsTestServer.join();
         } catch (Exception e) {
