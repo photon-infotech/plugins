825c7b0ed36a1655bbcac3e26b21e5541b2f915a
suresh-ma <suresh.marimuthu@photoninfotech.net>
2013-03-20 21:07:13 +0530

    Calabash Report for android

--- android-phresco-plugin/src/main/java/com/photon/phresco/plugins/android/RunAndroidTest.java
+++ android-phresco-plugin/src/main/java/com/photon/phresco/plugins/android/RunAndroidTest.java
@@ -42,6 +42,8 @@ public class RunAndroidTest implements PluginConstants {
 				builder.append(STR_SPACE);
 				String directory = project.getBuild().getDirectory();
 				builder.append(directory + File.separator + project.getArtifactId() + DOT + POMConstants.APK);	
+				builder.append(STR_SPACE);
+				builder.append("--format html --out calabashReport.html");
 				Utility.executeStreamconsumer(builder.toString(), project.getBasedir() + File.separator + workingDir);
 				return;
 			}
