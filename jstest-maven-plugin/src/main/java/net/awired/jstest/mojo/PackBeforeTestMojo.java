2fc08dcc884ec72ed297597a34f99d8403459ee1
suresh-ma <suresh.marimuthu@photoninfotech.net>
2013-03-27 20:57:14 +0530

    pack before mojo implementation

--- jstest-maven-plugin/src/main/java/net/awired/jstest/mojo/PackBeforeTestMojo.java
+++ jstest-maven-plugin/src/main/java/net/awired/jstest/mojo/PackBeforeTestMojo.java
@@ -1,20 +1,3 @@
-/**
- * JsTest Maven Plugin
- *
- * Copyright (C) 1999-2013 Photon Infotech Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package net.awired.jstest.mojo;
 
 import java.net.URL;
@@ -36,12 +19,11 @@ import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 
 /**
- * @component
- * @goal test
- * @phase test
- * @execute lifecycle="jstest-lifecycle" phase="process-test-resources""
+ * @goal pack-before-test
+ * @phase integration-test
+ * @execute lifecycle="jstest-lifecycle" phase="integration-test"
  */
-public class TestMojo extends AbstractJsTestMojo {
+public class PackBeforeTestMojo extends AbstractJsTestMojo {
 
     private static final String ERROR_MSG = "There are test failures.\n\nPlease refer to %s for the individual test results.";
 
