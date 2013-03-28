de549a0a2d053a67b8bbbcd9e601fd1dfeea6f06
vivekraja-v <vivekraja.vasudevan@photoninfotech.net>
2013-03-28 18:15:51 +0530

    copyright removed from js files

--- phresco-maven-plugin/src/test/projects/PHR_YUIHelloworld/src/main/webapp/lib/yui/tests/event/tests/manual/test-event-outside.js
+++ phresco-maven-plugin/src/test/projects/PHR_YUIHelloworld/src/main/webapp/lib/yui/tests/event/tests/manual/test-event-outside.js
@@ -1,20 +1,3 @@
-/*
- * Phresco Maven Plugin
- *
- * Copyright (C) 1999-2013 Photon Infotech Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 YUI({ filter: 'raw' }).use('gallery-event-konami', 'event-focus', 'event-outside', function (Y) {
     var selectors = [
             'blur','change','click','dblclick','focus','keydown','keypress',
