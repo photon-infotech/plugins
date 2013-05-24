4c217223227326aab2ded7e29f0de3fac033f3c3
kaleeswaran <kaleeswaran.subbaiya@photoninfotech.net>
2013-05-24 13:56:09 +0530

    artf684640: CST Task : Table of contents in the PDF summary should be tried with the inclusion of page numbers.

--- report-phresco-plugin/src/test/java/com/photon/phresco/plugins/GenerateReportTest.java
+++ report-phresco-plugin/src/test/java/com/photon/phresco/plugins/GenerateReportTest.java
@@ -42,6 +42,11 @@ public class GenerateReportTest extends TestCase implements PluginConstants {
 	protected void tearDown() throws Exception {
 	}
 	
+	@Test
+	public void test()  throws Exception {
+		System.out.println("Test case");
+	}
+	
 //	@Test
 	public void PdfWithImageAsStream() throws Exception {
 		try {
