79bcdf8a187224e890cdac1f0d6529e18d6d35c3
loheswaran_g <loheswaran.gopalsamy@photoninfotech.net>
2013-07-17 10:59:25 +0530

    qunit js test executor changed to PhantomJS

---
+++ jstest-maven-plugin/src/main/java/net/awired/jstest/executor/Executor.java
@@ -0,0 +1,17 @@
+package net.awired.jstest.executor;
+
+import java.io.File;
+
+import org.apache.maven.plugin.logging.Log;
+
+public interface Executor {
+    
+    void execute(String url) throws Exception;
+    
+    void close();
+
+    void setTargetSrcDir(File targetSourceDirectory);
+    
+    void setLog(Log log);
+
+}
