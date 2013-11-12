4f821edcc1f8578feb7e63efa4bc9f97a9545d52
jagadeesh-r <jagadeesh.reddy@photoninfotech.net>
2013-11-12 15:57:30 +0530

    [artf822404] : WU-Framework-3.1:unable to Download Generated PDF Report for Performance and Load test in Quality tab

---
+++ report-phresco-plugin/src/main/java/com/photon/phresco/plugins/Images.java
@@ -0,0 +1,21 @@
+package com.photon.phresco.plugins;
+
+public class Images {
+  
+	 String imageName;
+	 String image;
+	 
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+}
