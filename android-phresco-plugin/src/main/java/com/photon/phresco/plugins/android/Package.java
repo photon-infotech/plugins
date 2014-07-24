e961c8018f8b87dddb839c19b79a12bb2250c41a
varunkumar-s <varunkumar.sivasundaram@photoninfotech.net>
2014-07-24 12:42:29 +0530

    Android Hybrid splitted project build from CI phase issue fixed.

--- android-phresco-plugin/src/main/java/com/photon/phresco/plugins/android/Package.java
+++ android-phresco-plugin/src/main/java/com/photon/phresco/plugins/android/Package.java
@@ -271,7 +271,8 @@ public class Package implements PluginConstants {
 					rootModulePath = mavenProjectInfo.getBaseDir().getPath();
 				}
 				
-				PomProcessor pomProcessor = Utility.getPomProcessor(rootModulePath, moduleName);
+				File pomFile = pu.getPomFile(new File(dotPhrescoDir), new File(baseDir));
+				PomProcessor pomProcessor = new PomProcessor(pomFile);
 				String configJsonPath = pomProcessor.getProperty(Constants.POM_PROP_KEY_CONFIG_JSON_PATH);
 				if (StringUtils.isNotEmpty(configJsonPath)) {
 					StringBuilder path = new StringBuilder(mavenProjectInfo.getBaseDir().getPath());
