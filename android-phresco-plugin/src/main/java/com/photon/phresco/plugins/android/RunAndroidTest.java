d9806463f313ff78f6b3b113c5c54f05279283e0
viji <viji.ramasamy@photoninfotech.net>
2014-07-31 18:53:37 +0530

    [artf687986]:On unit and functional Tab we need device selection option like performance

--- android-phresco-plugin/src/main/java/com/photon/phresco/plugins/android/RunAndroidTest.java
+++ android-phresco-plugin/src/main/java/com/photon/phresco/plugins/android/RunAndroidTest.java
@@ -21,30 +21,22 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.cli.CommandLineException;
 import org.codehaus.plexus.util.cli.Commandline;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import com.perforce.p4java.Log;
 import com.photon.phresco.exception.PhrescoException;
 import com.photon.phresco.plugin.commons.MavenProjectInfo;
 import com.photon.phresco.plugin.commons.PluginConstants;
 import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration;
-import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter;
-import com.photon.phresco.plugins.model.Mojos.Mojo.Configuration.Parameters.Parameter.MavenCommands.MavenCommand;
 import com.photon.phresco.plugins.util.MojoUtil;
 import com.photon.phresco.service.pom.POMConstants;
 import com.photon.phresco.util.Constants;
 import com.photon.phresco.util.Utility;
-import com.phresco.pom.exception.PhrescoPomException;
-import com.phresco.pom.util.PomProcessor;
 
 public class RunAndroidTest implements PluginConstants {
 	static String currentWorkingDir;
@@ -92,9 +84,12 @@ public class RunAndroidTest implements PluginConstants {
 			sb.append(STR_SPACE);
 			sb.append(MVN_PHASE_INSTALL);
 			
-			String deviceValue = configs.get(DEVICES);
-			String signing = configs.get(SIGNING);
-			String device = configs.get(DEVICES_LIST);
+		    String signing = configs.get(SIGNING);
+			String deviceList = configs.get(DEVICES_LIST);
+			
+			
+			sb.append(STR_SPACE);
+			sb.append(HYPHEN_D + ANDROID_DEVICE + EQUAL + deviceList);
 			
 			Boolean isSigning = Boolean.valueOf(signing);
 			System.out.println("isSigning . " + isSigning);
@@ -104,25 +99,6 @@ public class RunAndroidTest implements PluginConstants {
 				sb.append(PSIGN);
 			}
 			
-			String otherDiviceValue = configs.get(deviceValue);
-			List<Parameter> parameters = configuration.getParameters().getParameter();
-			for (Parameter parameter : parameters) {
-				if (parameter.getPluginParameter() != null && parameter.getPluginParameter().equals(PluginConstants.PLUGIN_PARAMETER)) {
-					List<MavenCommand> mavenCommands = parameter.getMavenCommands().getMavenCommand();
-					for (MavenCommand mavenCommand : mavenCommands) {
-						if(mavenCommand.getKey().equalsIgnoreCase(deviceValue)) {
-							sb.append(STR_SPACE);
-							sb.append(mavenCommand.getValue());
-						} 
-					}
-				}
-				if(parameter.getKey().equalsIgnoreCase(deviceValue)) {
-					sb.append(STR_SPACE);
-					sb.append(HYPHEN_D + ANDROID_DEVICE + EQUAL + otherDiviceValue);
-				}
-			}
-			sb.append(STR_SPACE);
-			sb.append(HYPHEN_D + ANDROID_EMULATOR + EQUAL + DEFAULT);
 			File workingFile = new File(currentWorkingDir + File.separator + project.getFile().getName());
 			Log.info("workingFile :"+workingFile);
 		    if(workingFile.exists()) {
@@ -133,13 +109,7 @@ public class RunAndroidTest implements PluginConstants {
 			}
 			System.out.println("Command " + sb.toString());
 			Commandline commandline = new Commandline(sb.toString());
-			if(StringUtils.isNotEmpty(device)) {
-				PomProcessor pomProcessor = new PomProcessor(new File(baseDir + File.separator + PluginConstants.TEST_FOLDER + File.separator + PluginConstants.FUNCTIONAL_TEST_FOLDER + File.separator + Constants.POM_NAME));						
-				com.phresco.pom.model.PluginExecution.Configuration pluginExecutionConfiguration = pomProcessor.getPluginExecutionConfiguration(PluginConstants.MVN_ANT_PLUGIN_GRP_ID,PluginConstants.MVN_ANT_PLUGIN_ARTF_ID);
-				Element run = getTagname(PluginConstants.RUN, pluginExecutionConfiguration);			
-				run.setAttribute(PluginConstants.ADB_SERIAL, device);
-				pomProcessor.save();
-			}
+			
 			if (StringUtils.isNotEmpty(workingDir)) {
 				commandline.setWorkingDirectory(currentWorkingDir);
 			}
@@ -153,26 +123,8 @@ public class RunAndroidTest implements PluginConstants {
 			throw new PhrescoException(e);
 		} catch (IOException e) {
 			throw new PhrescoException(e);
-		} catch (PhrescoPomException e) {
-			// TODO Auto-generated catch block
-			throw new PhrescoException(e);
-		}
+		} 
 	}
 	
-	private static Element getTagname(String tagName,
-			com.phresco.pom.model.PluginExecution.Configuration configuration) {
-		for (Element config : configuration.getAny()) {
-			NodeList childNodes = config.getChildNodes();
-			for(int i=0 ; i< childNodes.getLength(); i ++) {
-				Node node = childNodes.item(i);
-				if(node instanceof Element) {
-					Element element = (Element) node;
-					if(tagName.equals(element.getTagName())){
-						return element;
-					}
-				}
-			}
-		}
-		return null;
-	}
+	
 }
